45e89cfb3e8d3e5baab2e260a1549049e74ff16b
michaelzoidl <michael@zoidl.de>
2015-11-13 14:33:33 +0100

    Update main module

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,12 +1,22 @@
 import request from 'request';
 import cheerio from 'cheerio';
 import iconv from 'iconv-lite';
+import readlineSync from 'readline-sync';
+import ParseTable from './utils/parseTable';
 
-const RequestURL = [
+let startStation = undefined;
+
+if(process.argv && process.argv[2]) {
+  startStation = process.argv[2];
+} else {
+  startStation = readlineSync.question('From what station do you want to start? : ')
+}
+
+const RequestURI = [
   `https://`,  // protocol
   `www.mvg-live.de`,  // url
   `/ims/dfiStaticAuswahl.svc`, // endpoint
-  `?haltestelle=Hauptbahnhof`, // station of interesst
+  `?haltestelle=${startStation}`, // station of interesst
   `&ubahn=checked`, // look for subway
   `&bus=checked`, // look for bus
   `&tram=checked`, // look for tram
@@ -14,14 +24,13 @@ const RequestURL = [
 ].join('');
 
 request({
-  uri: RequestURL,
+  uri: RequestURI,
   encoding: null
 }, (err, res, body) => {
-  if(err && res.statusCode !== 200) throw 'Unexpected Error';
-  // console.log(body);
+  if(err && res.statusCode !== 200) throw 'Error: Had problems to connect to mvg-page';
 
-  var utf8String = iconv.decode(new Buffer(body), "ISO-8859-1");
-  console.log(utf8String);
+  const utf8String = iconv.decode(new Buffer(body), "ISO-8859-1");
+  console.log(ParseTable(utf8String).departures);
 
   // const $ = cheerio.load(body);
   // console.log($('table').html());
