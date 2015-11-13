f2fd0fbb2ac4ab15acdcb76b73f0411d245ff05e
michaelzoidl <michael@zoidl.de>
2015-11-13 11:42:28 +0100

    Update main code

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,1 +1,12 @@
-console.log('hi');
+import request from 'request';
+import cheerio from 'cheerio';
+
+
+request('https://www.mvg-live.de/ims/dfiStaticAuswahl.svc?haltestelle=Goetheplatz&ubahn=checked&bus=checked&tram=checked&sbahn=checked', (err, res, body) => {
+  if(err && res.statusCode !== 200) throw 'Unexpected Error';
+
+  const $ = cheerio.load(body);
+
+  console.log($('table').html());
+
+});
