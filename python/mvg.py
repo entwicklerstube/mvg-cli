44ec365a51bcf4552f46d6918ca13992c9269330
Jan Schmidle <jan@cospired.com>
2015-12-02 12:07:19 +0100

    adding no color mode and fixing colored output

--- python/mvg.py
+++ python/mvg.py
@@ -12,7 +12,31 @@ import signal
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+color = True
+
+def fgc(p):
+    global color
+    if color:
+        return fg(p)
+    else:
+        return ''
+
+def bgc(p):
+    global color
+    if color:
+        return bg(p)
+    else:
+        return ''
+
+def attrc(p):
+    global color
+    if color:
+        return attr(p)
+    else:
+        return ''
+
 def signal_handler(signal, frame):
+    print attrc('reset')
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -46,7 +70,7 @@ def printStations(station, hide):
         if truc.text:
             table_time.append(truc.text.encode('latin-1').strip())
 
-    print ('%s%s From %s %s' % (fg('black'), bg('white'), station, attr('reset')))
+    print ('%s%s From %s %s' % (fgc('black'), bgc('white'), station, attrc('reset')))
 
     for i in range(1,len(table_line)):
         if table_line[i][0]=="U":
@@ -60,45 +84,45 @@ def printStations(station, hide):
 
     if len(ubahn)>0 or not hide:
         print '\n'
-        print ('%s%s Ubahn %s' % (fg('white'), bg('yellow'), attr('reset')))
+        print ('%s%s Ubahn %s' % (fgc('white'), bgc('yellow'), attrc('reset')))
         if len(ubahn)==0:
-                print ('%s No Ubahn from this station %s' % (fg('yellow'), attr('res_bold')))
+                print ('%s No Ubahn from this station %s' % (fgc('yellow'), attrc('reset')))
         for idx, x in enumerate(ubahn):
             if idx == 0:
-                print ('%s%s %s %s' % (fg('white'), bg('red'), x, attr('reset')))
+                print ('%s%s %s %s' % (fgc('white'), bgc('red'), x, attrc('reset')))
             else:
                 print ' ' + x
 
     if len(sbahn)>0 or not hide:
         print '\n'
-        print ('%s%s Sbahn %s' % (fg('white'), bg('green'), attr('reset')))
+        print ('%s%s Sbahn %s' % (fgc('white'), bgc('green'), attrc('reset')))
         if len(sbahn)==0:
-                print ('%s No Sbahn from this station %s' % (fg('yellow'), attr('res_bold')))
+                print ('%s No Sbahn from this station %s' % (fgc('yellow'), attrc('reset')))
         for idx, x in enumerate(sbahn):
             if idx == 0:
-                print ('%s%s %s %s' % (fg('white'), bg('red'), x, attr('reset')))
+                print ('%s%s %s %s' % (fgc('white'), bgc('red'), x, attrc('reset')))
             else:
                 print ' ' + x
 
     if len(tram)>0 or not hide:
         print '\n'
-        print ('%s%s Tram %s' % (fg('white'), bg('cyan'), attr('reset')))
+        print ('%s%s Tram %s' % (fgc('white'), bgc('cyan'), attrc('reset')))
         if len(tram)==0:
-                print ('%s No Tram from this station %s' % (fg('yellow'), attr('res_bold')))
+                print ('%s No Tram from this station %s' % (fgc('yellow'), attrc('reset')))
         for idx, x in enumerate(tram):
             if idx == 0:
-                print ('%s%s %s %s' % (fg('white'), bg('red'), x, attr('reset')))
+                print ('%s%s %s %s' % (fgc('white'), bgc('red'), x, attrc('reset')))
             else:
                 print ' ' + x
 
     if len(bus)>0 or not hide:
         print '\n'
-        print ('%s%s Bus %s' % (fg('white'), bg('blue'), attr('reset')))
+        print ('%s%s Bus %s' % (fgc('white'), bgc('blue'), attrc('reset')))
         if len(bus)==0:
-                print ('%s No Bus/Tram from this station %s' % (fg('yellow'), attr('res_bold')))
+                print ('%s No Bus/Tram from this station %s' % (fgc('yellow'), attrc('reset')))
         for idx, x in enumerate(bus):
             if idx == 0:
-                print ('%s%s %s %s' % (fg('white'), bg('red'), x, attr('reset')))
+                print ('%s%s %s %s' % (fgc('white'), bgc('red'), x, attrc('reset')))
             else:
                 print ' ' + x
 
@@ -106,15 +130,17 @@ def printHelp():
     print 'mvg.py -s <station>'
     print '       -t : refresh every 10 sec'
     print '       -x : hide empty lists'
+    print '       -c : no color'
     print '       -h : this help'
 
 def main(argv):
+    global color
     station = ''
     loop = False
     hide = False
 
     try:
-        opts, args = getopt.getopt(argv,"htxs:", ['station='])
+        opts, args = getopt.getopt(argv,"htxcs:", ['station='])
     except getopt.GetoptError:
         printHelp()
         sys.exit(2)
@@ -126,6 +152,8 @@ def main(argv):
             loop = True
         elif opt in ("-x"):
             hide = True
+        elif opt in ("-c"):
+            color = False
         elif opt in ("-s", "--station"):
             station = arg
 
