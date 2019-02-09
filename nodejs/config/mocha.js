417999ec283707f6e56ca139a39cd9ec3b43ba77
Michael Zoidl <github@michaelzoidl.com>
2019-02-09 23:59:34 +0100

    purge

--- nodejs/config/mocha.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
-
-chai.use(sinonChai);
-
-global.chai = chai;
-global.expect = chai.expect;
-global.sinon = sinon;
